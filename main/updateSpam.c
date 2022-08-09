9e8d09fa63db1bbac6faa9fd98f00b358e6a9cfe
sharmavins23 <sharmavins23@gmail.com>
2022-08-08 22:53:25 -0400

    fixing some errors

--- main/updateSpam.c
+++ main/updateSpam.c
@@ -12,58 +12,6 @@
 
 #define TAG "UPDATE SPAM"
 
-// ===== HTTP Client Functionality =============================================
-
-esp_http_client_handle_t setupHTTPClient() {
-    // Create a new HTTP client
-    esp_http_client_config_t config = {
-        .url = "http://vinsdev.ml:8080/update",  // Server endpoint
-        .method = HTTP_METHOD_POST,              // POST connection
-    };
-
-    // Start the HTTP session
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    // Set the request header to specify JSON content towards the server
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-
-    ESP_LOGI(TAG, "HTTP client setup complete.");
-
-    // Return the client object to be used in other functions
-    return client;
-}
-
-void destroyHTTPClient(esp_http_client_handle_t client) {
-    // Close the connection
-    esp_http_client_close(client);
-    // Clean up any other objects
-    esp_http_client_cleanup(client);
-
-    ESP_LOGI(TAG, "HTTP client successfully destroyed.");
-
-    return;
-}
-
-// Note: You are responsible for cleaning up your own POST data
-void performHTTPPOST(esp_http_client_handle_t client) {
-    // Get the post data
-    char *postData = createJSON();  // Might slow down the HTTP post speed
-
-    // Set the POST field data
-    esp_http_client_set_post_field(client, postData, strlen(postData));
-
-    // Perform the HTTP POST
-    esp_err_t error = esp_http_client_perform(client);
-
-    // Check for errors
-    if (error != ESP_OK) {
-        ESP_LOGE(TAG, "HTTP POST failed: %s", esp_err_to_name(error));
-    } else {
-        ESP_LOGI(TAG, "Sent HTTP POST request.");
-    }
-
-    return;
-}
-
 // ===== JSON formatting =======================================================
 
 // // Create a JSON string based on a scraped MAC ID
@@ -146,6 +94,36 @@ float getESPTemperature() {
     return temperature;
 }
 
+// Given an ESP reset reason, convert it to a string
+char *resetReasonToString(esp_reset_reason_t reason) {
+    switch (reason) {
+        case ESP_RST_UNKNOWN:
+            return "Reset reason can not be determined";
+        case ESP_RST_POWERON:
+            return "Reset due to power-on event";
+        case ESP_RST_EXT:
+            return "Reset by external pin (not applicable for ESP32)";
+        case ESP_RST_SW:
+            return "Software reset via esp_restart";
+        case ESP_RST_PANIC:
+            return "Software reset due to exception/panic";
+        case ESP_RST_INT_WDT:
+            return "Reset (software or hardware) due to interrupt watchdog";
+        case ESP_RST_TASK_WDT:
+            return "Reset due to task watchdog";
+        case ESP_RST_WDT:
+            return "Reset due to other watchdogs";
+        case ESP_RST_DEEPSLEEP:
+            return "Reset after exiting deep sleep mode";
+        case ESP_RST_BROWNOUT:
+            return "Brownout reset (software or hardware)";
+        case ESP_RST_SDIO:
+            return "Reset over SDIO";
+        default:
+            return "Reset reason is unspecified in documentation";
+    }
+}
+
 // Create a JSON string, combining the proper parameters and values
 char *createJSON() {
     // Create a JSON object
@@ -171,7 +149,7 @@ char *createJSON() {
     // Get the last reset reason
     esp_reset_reason_t resetReason = esp_reset_reason_get();
     // Convert the reset reason to a string
-    char *resetReasonString = esp_reset_reason_to_string(resetReason);
+    char *resetReasonString = resetReasonToString(resetReason);
     // Add the reset reason to the JSON object
     cJSON_AddItemToObject(json, "resetReason",
                           cJSON_CreateString(resetReasonString));
@@ -184,6 +162,58 @@ char *createJSON() {
 
     // Return our created JSON object
     return jsonString;
+}
+
+// ===== HTTP Client Functionality =============================================
+
+esp_http_client_handle_t setupHTTPClient() {
+    // Create a new HTTP client
+    esp_http_client_config_t config = {
+        .url = "http://vinsdev.ml:8080/update",  // Server endpoint
+        .method = HTTP_METHOD_POST,              // POST connection
+    };
+
+    // Start the HTTP session
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    // Set the request header to specify JSON content towards the server
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+
+    ESP_LOGI(TAG, "HTTP client setup complete.");
+
+    // Return the client object to be used in other functions
+    return client;
+}
+
+void destroyHTTPClient(esp_http_client_handle_t client) {
+    // Close the connection
+    esp_http_client_close(client);
+    // Clean up any other objects
+    esp_http_client_cleanup(client);
+
+    ESP_LOGI(TAG, "HTTP client successfully destroyed.");
+
+    return;
+}
+
+// Note: You are responsible for cleaning up your own POST data
+void performHTTPPOST(esp_http_client_handle_t client) {
+    // Get the post data
+    char *postData = createJSON();  // Might slow down the HTTP post speed
+
+    // Set the POST field data
+    esp_http_client_set_post_field(client, postData, strlen(postData));
+
+    // Perform the HTTP POST
+    esp_err_t error = esp_http_client_perform(client);
+
+    // Check for errors
+    if (error != ESP_OK) {
+        ESP_LOGE(TAG, "HTTP POST failed: %s", esp_err_to_name(error));
+    } else {
+        ESP_LOGI(TAG, "Sent HTTP POST request.");
+    }
+
+    return;
 }
 
 // ===== Main functionality ====================================================
@@ -199,7 +229,8 @@ void spamUpdates() {
     int i = 30;
     while (i--) {
         // Perform the HTTP POST request
-        performHTTPPOST(client, postData);
+        // performHTTPPOST(client, postData);
+        performHTTPPost(client);
 
         // Delay the loop
         // vTaskDelay(TASK_TIME_MS / portTICK_PERIOD_MS);
